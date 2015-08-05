f78a90315fc1e08675c71f69850417ec16c13b25
phoenixstarhiro <phoenixstarhiro@users.noreply.github.com>
2015-08-05 10:36:44 -0700

    Add glog library.
    The revision is 0.3.4 release.
    The original glog code is retrieved from:
    https://github.com/google/glog/archive/v0.3.4.zip

--- tools/build_googlelib.py
+++ tools/build_googlelib.py
@@ -6,10 +6,11 @@ import subprocess
 
 
 _GOOGLE_LIB_PATH = 'googlelib'
-_DEPENDENT_PACKAGES = [
+_DEPENDENT_PACKAGE_LIST = [
     'cmake',
     'g++',
     'automake',
+    'libunwind8-dev',
 ]
 
 _ROOT_PATH = os.path.abspath(os.path.normpath(
@@ -27,16 +28,27 @@ def _safe_makedirs(path):
 
 def _install_deps():
     subprocess.check_call(
-        ['sudo', 'apt-get', 'install', 'cmake', 'g++', 'automake'])
+        ['sudo', 'apt-get', 'install'] + _DEPENDENT_PACKAGE_LIST)
 
 
 def _build_gflags():
     gflags_path = os.path.join(_ROOT_PATH, _GOOGLE_LIB_PATH, 'gflags')
     _safe_makedirs(gflags_path)
+    environ = os.environ.copy()
+    environ['CXXFLAGS'] = '-fPIC'  # PIC build, as it is required by glog.
     subprocess.check_call(['cmake', '../../third_party/gflags'],
-                          cwd=gflags_path)
-    subprocess.check_call(['make'],
-                          cwd=gflags_path)
+                          cwd=gflags_path,
+                          env=environ)
+    subprocess.check_call(['make'], cwd=gflags_path)
+
+
+def _build_glog():
+    glog_path = os.path.join(_ROOT_PATH, _GOOGLE_LIB_PATH, 'glog')
+    _safe_makedirs(glog_path)
+    subprocess.check_call(['../../third_party/glog/configure',
+                           '--with-gflags=../gflags'],
+                          cwd=glog_path)
+    subprocess.check_call(['make'], cwd=glog_path)
 
 
 def _parse_args():
@@ -52,6 +64,7 @@ def main():
     if not parsed_args.skip_install_deps:
         _install_deps()
     _build_gflags()
+    _build_glog()
 
 
 if __name__ == '__main__':
