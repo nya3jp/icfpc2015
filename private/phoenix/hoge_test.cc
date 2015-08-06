7e32fd1651635210361216d97f956d62bc427b3b
phoenixstarhiro <phoenixstarhiro@users.noreply.github.com>
2015-08-06 07:50:13 -0700

    Add gtest library.

---
+++ private/phoenix/hoge_test.cc
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+TEST(Hoge, testtest) {
+  EXPECT_EQ(1, 2);
+}
+
+TEST(Fuga, testtest) {
+  EXPECT_TRUE(true);
+}
--- tools/build_googlelib.py
+++ tools/build_googlelib.py
@@ -52,9 +52,17 @@ def _build_glog():
     subprocess.check_call(['make'], cwd=glog_path)
 
 
+def _build_gtest():
+    gtest_path = os.path.join(_ROOT_PATH, _GOOGLE_LIB_PATH, 'gtest')
+    _safe_makedirs(gtest_path)
+    subprocess.check_call(['cmake', '../../third_party/gtest'],
+                          cwd=gtest_path)
+    subprocess.check_call(['make'], cwd=gtest_path)
+
+
 def _parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--skip-install-deps', action='store_true',
+    parser.add_argument('--install-deps', action='store_true',
                         help='Do not run apt-get install for dependent '
                         'packages.')
     return parser.parse_args()
@@ -62,10 +70,11 @@ def _parse_args():
 
 def main():
     parsed_args = _parse_args()
-    if not parsed_args.skip_install_deps:
+    if parsed_args.install_deps:
         _install_deps()
     _build_gflags()
     _build_glog()
+    _build_gtest()
 
 
 if __name__ == '__main__':
