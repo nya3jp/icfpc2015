d674bf1de88d51b0cf394e364cb2a61daece5bf9
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2015-08-08 00:19:45 +0900

    Fix PYTHONPATH

--- tools/python.sh
+++ tools/python.sh
@@ -2,5 +2,5 @@
 
 root_dir="$(dirname "$0")/.."
 
-export PYTHONPATH="$root_dir/pylib:$root_dir/third_party/python-gflags:${PYTHONPATH:+":$PYTHONPATH"}"
+export PYTHONPATH="$root_dir/pylib:$root_dir/third_party/python-gflags-2.0:${PYTHONPATH:+":$PYTHONPATH"}"
 exec python "$@"
