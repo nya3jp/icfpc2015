14627ec3a75dd08b42cc64809590fd0aaf1a5f1e
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2015-08-08 00:42:57 +0900

    Submit scores to leaderboard.

---
+++ dashboard/submit.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import json
+import logging
+import subprocess
+import sys
+import tempfile
+
+import logging_util
+import gflags
+
+FLAGS = gflags.FLAGS
+
+gflags.DEFINE_string('api_key', None, 'API key')
+gflags.DEFINE_string('team_id', None, 'team ID')
+gflags.MarkFlagAsRequired('api_key')
+gflags.MarkFlagAsRequired('team_id')
+
+
+def main(argv):
+  logging_util.setup()
+
+  for solution_path in argv[1:]:
+    logging.info('Submitting: %s', solution_path)
+    with open(solution_path) as f:
+      problem_result = json.load(f)
+    for seed_result in problem_result:
+      for key in seed_result.keys():
+        if key.startswith('_'):
+          del seed_result[key]
+    with tempfile.NamedTemporaryFile() as f:
+      json.dump(problem_result, f, indent=2)
+      f.flush()
+      subprocess.check_call([
+        'curl', '--user', ':%s' % FLAGS.api_key,
+        '--header', 'Content-Type: application/json',
+        '--data', '@%s' % f.name,
+        'https://davar.icfpcontest.org/teams/%s/solutions' % FLAGS.team_id])
+
+
+if __name__ == '__main__':
+  sys.exit(main(FLAGS(sys.argv)))
--- jenkins/natsubate.sh
+++ jenkins/natsubate.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 export LANG=C
-
 cd "$(dirname "$0")/.."
+set -ex
+
+
+# Update dashboard
 
 args=()
 
@@ -13,4 +16,12 @@ for i in $(seq 0 99); do
   fi
 done
 
-exec tools/python.sh dashboard/update.py ${args[@]} --output_dir=out/dashboard --logtostderr=info
+tools/python.sh dashboard/update.py ${args[@]} --output_dir=out/dashboard --logtostderr=info
+
+
+# Submit to leaderboard
+
+api_key_path=/tmp/api_key.txt
+if [[ -f $api_key_path ]]; then
+  tools/python.sh dashboard/submit.py --logtostderr=info --api_key="$(cat $api_key_path)" --team_id=116 out/dashboard/solution_*_BEST.json
+fi
