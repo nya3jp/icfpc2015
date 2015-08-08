62d9dec3dceb0fc2a6e410f94e6dd4e2a18d4413
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2015-08-08 10:14:52 +0900

    Disable jenkins

--- jenkins/natsubate.sh
+++ jenkins/natsubate.sh
@@ -4,24 +4,3 @@ export LANG=C
 cd "$(dirname "$0")/.."
 set -ex
 
-
-# Update dashboard
-
-args=()
-
-for i in $(seq 0 99); do
-  path="problems/problem_$i.json"
-  if [[ -f "$path" ]]; then
-    args+=(-f $path)
-  fi
-done
-
-tools/python.sh dashboard/update.py ${args[@]} --output_dir=out/dashboard --logtostderr=info
-
-
-# Submit to leaderboard
-
-api_key_path=/tmp/api_key.txt
-if [[ -f $api_key_path ]]; then
-  tools/python.sh dashboard/submit.py --logtostderr=info --api_key="$(cat $api_key_path)" --team_id=116 out/dashboard/solution_*_BEST.json
-fi
