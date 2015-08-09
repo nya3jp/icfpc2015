8c25406575cb29db1847f5b2146372333c62d9cf
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2015-08-09 15:12:54 +0900

    Minor fix

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
+set -ex
+
 sudo apt-get install -y build-essential cmake g++ automake libunwind8-dev python-dev python-pip
 pip install --user -r requirements.txt
