1772ecd1c624e682162838f7e4b703ed06166340
Shun Sakuraba <shun.sakuraba+calloficfpc@gmail.com>
2015-08-09 12:59:36 +0000

    Reachability count

--- simulator/ai_util.cc
+++ simulator/ai_util.cc
@@ -1,6 +1,8 @@
 #include "ai_util.h"
 
 #include <vector>
+#include <stack>
+#include <utility>
 
 #include "game.h"
 
@@ -33,4 +35,52 @@ int64_t GetHeightPenalty(const std::vector<int>& height) {
 
 int64_t GetHeightPenaltyFromGame(const Game& game) {
   return GetHeightPenalty(GetHeightLine(game));
+}
+
+int64_t GetDotReachabilityFromTop(const Game& game)
+{
+  int64_t ret = 0;
+  int width = game.GetBoard().width();
+  int height = game.GetBoard().height();
+
+  int movepattern[] = { 0, -1, 
+                        1, 0,
+                        0, 1,
+                        -1, 0 };
+
+  std::vector<bool> visited(width * height, false);
+  const Board::Map& cells = game.GetBoard().cells();
+
+  std::stack<std::pair<int, int> > dfs;
+  
+  for(int i = 0; i < width; ++i) {
+    if(!cells[0][i]) {
+      dfs.push(std::make_pair(i, 0));
+    }
+  }
+
+  while(!dfs.empty()) {
+    int x = dfs.top().first;
+    int y = dfs.top().second;
+    dfs.pop();
+    if(visited[y * width + x]) continue;
+    ret++;
+    visited[y * width + x] = true;
+    
+    for(int i = 0; i < 8; i += 2) {
+      int newx = x + movepattern[i];
+      int newy = y + movepattern[i + 1];
+      
+      if((newx < 0) || (newx >= width) || 
+         (newy < 0) || (newy >= height)) {
+        continue;
+      }
+      if(cells[newy][newx] || visited[newy * width + newx]) {
+        continue;
+      }
+      dfs.push(std::make_pair(newx, newy));
+    }
+  }
+
+  return ret;
 }
--- simulator/ai_util.h
+++ simulator/ai_util.h
@@ -8,4 +8,13 @@
 std::vector<int> GetHeightLine(const Game& game);
 int64_t GetHeightPenalty(const std::vector<int>& height);
 int64_t GetHeightPenaltyFromGame(const Game& game);
+
+/**
+   Returns the number of points reachable from the top of the board
+
+   @return The number of reachable points from the top
+ */
+int64_t GetDotReachabilityFromTop(const Game& game);
+
+
 #endif  // AI_UTIL_H__
