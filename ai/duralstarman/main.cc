aa5c6f414e80981aec7baa576983ec159f10ffe2
Shun Sakuraba <shun.sakuraba+calloficfpc@gmail.com>
2015-08-10 11:52:55 +0000

    ds variants

--- ai/duralstarman/main.cc
+++ ai/duralstarman/main.cc
@@ -13,6 +13,12 @@ int main(int argc, char* argv[]) {
 
   FLAGS_logtostderr = true;
   KaminekoScorer base_scorer;
+
+#ifdef FIXED_WIDTH
+  FLAGS_dural_depth = FIXED_WIDTH;
+  FLAGS_dural_width = 32;
+#endif
+
   return RunSolver(
       new DuralStarmanSolver(&base_scorer, FLAGS_dural_width, FLAGS_dural_depth),
       "DuralStarman");
